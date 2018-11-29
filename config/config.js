5667ecd18163f176d2411f63b6fea2e79b592b95
nogo6260 <15857356260@163.com>
2018-11-29 15:56:23 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,14 @@
+export default {
+    singular: true,
+    //插件
+    plugins: [
+        ['umi-plugin-react',{
+
+        }]
+    ],
+    //路由
+    routes:[{
+        path: '/',
+        component: './HelloWorld',
+    }]
+};
